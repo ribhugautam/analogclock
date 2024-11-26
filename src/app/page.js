5c97541f96adc66f8e884e5975e6c03fc9d9e853
ribhugautam <gautamribhu@gmail.com>
2024-11-26 19:49:48 +0530

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,66 @@
-import Image from "next/image";
+"use client";
+import React, { useEffect } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [timeZone, setTimeZone] = React.useState("UTC");
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+  function updateClock() {
+    const date = new Date();
+
+    const time = Intl.DateTimeFormat("en-US", {
+      timeZone: timeZone,
+      hour: "numeric",
+      minute: "numeric",
+      second: "numeric",
+    }).format(date);
+
+    const [hours, minutes, sec] = time.split(":");
+    const seconds = sec.substring(0, 2);
+
+    document.getElementById("hour").style.transform = `rotate(${
+      hours * 30
+    }deg)`;
+    document.getElementById("minutes").style.transform = `rotate(${
+      minutes * 6
+    }deg)`;
+    document.getElementById("seconds").style.transform = `rotate(${
+      seconds * 6
+    }deg)`;
+  }
+
+  useEffect(() => {
+    updateClock();
+    const interval = setInterval(() => {
+      updateClock();
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [timeZone]);
+
+  return (
+    <div className="h-screen bg-black w-screen flex flex-col items-center gap-10 p-10 justify-center">
+
+      <h1 className="text-white font-bold text-3xl" >ANALOG CLOCK</h1>
+
+      <div className="flex flex-col gap-2 justify-center items-center">
+        <h3 className="text-white font-bold">Time Zone</h3>
+
+        <select
+          onChange={(e) => setTimeZone(e.target.value)}
+          className="text-black px-2 rounded-xl"
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          <option value="UTC">Local Time</option>
+          <option value="Europe/Andorra">Europe/Andorra</option>
+          <option value="Asia/Dubai">Asia/Dubai</option>
+          <option value="America/La_Paz">America/La_Paz</option>
+          <option value="Africa/El_Aaiun">Africa/El_Aaiun</option>
+        </select>
+      </div>
+
+      <div className="clock__face">
+        <div id="hour" className="hand hand--hours" />
+        <div id="minutes" className="hand hand--minutes" />
+        <div id="seconds" className="hand hand--seconds" />
+      </div>
     </div>
   );
 }
