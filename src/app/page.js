006e0586bea2f38cc3e793988be61c34a98f8efc
ribhugautam <gautamribhu@gmail.com>
2024-12-01 19:07:31 +0530

    Fixed rerender issue

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,13 @@
 "use client";
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { timeZones } from "@/lib/timeZone";
 
 export default function Home() {
   const [timeZone, setTimeZone] = React.useState("UTC");
+
+  const hourRef = useRef(null);
+  const minuteRef = useRef(null);
+  const secondRef = useRef(null);
 
   function updateClock() {
     const date = new Date();
@@ -18,30 +22,18 @@ export default function Home() {
     const [hours, minutes, sec] = time.split(":");
     const seconds = sec.substring(0, 2);
 
-    document.getElementById("hour").style.transform = `rotate(${
-      hours * 30
-    }deg)`;
-    document.getElementById("minutes").style.transform = `rotate(${
-      minutes * 6
-    }deg)`;
-    document.getElementById("seconds").style.transform = `rotate(${
-      seconds * 6
-    }deg)`;
+    if (hourRef.current && minuteRef.current && secondRef.current) {
+      hourRef.current.style.transform = `rotate(${hours * 30}deg)`;
+      minuteRef.current.style.transform = `rotate(${minutes * 6}deg)`;
+      secondRef.current.style.transform = `rotate(${seconds * 6}deg)`;
 
-    seconds === "00"
-      ? (document.getElementById("seconds").style.transition = "none")
-      : (document.getElementById("seconds").style.transition =
-          "all 75ms ease-in-out");
-
-    minutes === "00"
-      ? (document.getElementById("minutes").style.transition = "none")
-      : (document.getElementById("minutes").style.transition =
-          "all 75ms ease-in-out");
-
-    hours === "00"
-      ? (document.getElementById("hours").style.transition = "none")
-      : (document.getElementById("hours").style.transition =
-          "all 75ms ease-in-out");
+      secondRef.current.style.transition =
+        seconds === "00" ? "none" : "all 75ms ease-in-out";
+      minuteRef.current.style.transition =
+        minutes === "00" ? "none" : "all 75ms ease-in-out";
+      hourRef.current.style.transition =
+        hours === "00" ? "none" : "all 75ms ease-in-out";
+    }
   }
 
   useEffect(() => {
@@ -70,25 +62,25 @@ export default function Home() {
         </select>
       </div>
       <div className="clock__face rounded-full">
-        <div className="aspect-square h-[1.2rem] absolute Fmx-auto z-10 bottom-0 top-0 my-auto bg-[#ffffff] shadow-[0_0_1rem_#ff0000] rounded-full " />
+        <div className="aspect-square h-[1.2rem] absolute mx-auto z-10 bottom-0 top-0 my-auto bg-[#ffffff] shadow-[0_0_1rem_#ff0000] rounded-full" />
         <h3 className="text-white text-2xl font-bold ">12</h3>
-        <h3 className="text-white font-bold text-2xl my-auto mx-auto right-0  top-[44%] absolute ">
+        <h3 className="text-white font-bold text-2xl my-auto mx-auto right-0 top-[44%] absolute">
           03
         </h3>
-        <h3 className="text-white font-bold text-2xl my-auto mx-auto left-0  top-[44%] absolute ">
+        <h3 className="text-white font-bold text-2xl my-auto mx-auto left-0 top-[44%] absolute">
           09
         </h3>
         <h3 className="text-white font-bold text-2xl bottom-0 absolute ">06</h3>
         <div
-          id="hour"
+          ref={hourRef}
           className="hand hand--hours origin-bottom rounded-full"
         />
         <div
-          id="minutes"
+          ref={minuteRef}
           className="hand hand--minutes origin-bottom rounded-full"
         />
         <div
-          id="seconds"
+          ref={secondRef}
           className="hand hand--seconds origin-bottom rounded-full"
         />
       </div>
